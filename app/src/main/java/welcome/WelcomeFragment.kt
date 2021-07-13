72a52d359487a9438b301df23a60578b93b13e58
Daria Bak <daria.bak22@gmail.com>
2021-07-13 16:02:14 +0100

    Added some views to welcome page

--- app/src/main/java/welcome/WelcomeFragment.kt
+++ app/src/main/java/welcome/WelcomeFragment.kt
@@ -17,14 +17,20 @@ class WelcomeFragment: Fragment() {
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_welcome_layout, container, false)
         val submitButton = rootView.findViewById<View>(R.id.submit_button) as Button
-        val spinner = rootView.findViewById<View>(R.id.number_of_teams_spinner) as Spinner
-        var items = arrayOf( 2, 3, 4, 5, 6, 7, 8, 9, 10, 18)
+        val numberOfTeamsSpinner = rootView.findViewById<View>(R.id.number_of_teams_spinner) as Spinner
+        var items = arrayOf( 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
         val adapter: ArrayAdapter<Int> = ArrayAdapter<Int>(requireActivity(), android.R.layout.simple_spinner_item, items)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter
+        numberOfTeamsSpinner.adapter = adapter
+
+        val typeOfScheduleSpinner = rootView.findViewById<View>(R.id.type_of_schedule_spinner) as Spinner
+        var types = arrayOf("Tournament Single Elimination")
+        val typeAdapter: ArrayAdapter<String> = ArrayAdapter<String>(requireActivity(), android.R.layout.simple_spinner_item, types)
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        typeOfScheduleSpinner.adapter = typeAdapter
 
         submitButton.setOnClickListener{
-          var numberOfTeams =  spinner.selectedItem as Int
+          var numberOfTeams =  numberOfTeamsSpinner.selectedItem as Int
             val action = WelcomeFragmentDirections.actionWelcomeToBrackets()
             action.numberOfTeams = numberOfTeams
             rootView.findNavController().navigate(action)
